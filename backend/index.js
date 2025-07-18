03f0b86161fff0123e26802ef883adf328c33628
Subash <subashmurugan2021@gmail.com>
2025-07-18 12:31:46 +0530

    update the cors

--- backend/index.js
+++ backend/index.js
@@ -16,13 +16,24 @@ const PORT = process.env.PORT;
 
 
 app.use(express.json());
-app.use(cors());
 
-// app.use(cors({
-//     origin: "https://sathya-fashions.vercel.app",
-//     methods: ["GET", "POST","DELETE"],
-//     credentials: true, 
-//   },));
+const allowedOrigins = [
+    'http://localhost:5173',
+    `${process.env.FRONEND_URL}`
+  ];
+  
+  app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,POST',
+    allowedHeaders: '*',
+    credentials: true,
+  }));
 
 
 
